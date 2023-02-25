5d04b2e4c5294f5a9bdcd651911d3874ffc3b891
eMatheux <neguinavonts12@gmail.com>
2023-02-24 22:22:30 -0300

    uploaded

---
+++ src/main/java/com/ag/armazem/Armazem.java
@@ -0,0 +1,15 @@
+package com.ag.armazem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.*;
+
+public final class Armazem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
